a0ddb50e55a0d6e21991e0180efb4e1ff2db1aff
Ran Shi <ranshi4396@outlook.com>
2023-07-01 03:12:46 -0400

    first commit

---
+++ dotfiles/README.md
@@ -0,0 +1,5 @@
+# dotfiles
+
+Dependencies:
+- tmux-plugins https://github.com/tmux-plugins/tpm
+- vim-plug https://github.com/junegunn/vim-plug
---
+++ dotfiles/setup.sh
@@ -0,0 +1,10 @@
+wd=$(pwd)
+
+ln -s $wd/tmux.conf $HOME/.tmux.conf
+ln -s $wd/vim/colors $HOME/.vim/colors
+ln -s $wd/vimrc $HOME/.vimrc
+ln -s $wd/i3 $HOME/.config/i3
+ln -s $wd/kitty $HOME/.config/kitty
+ln -s $wd/picom $HOME/.config/picom
+
+
